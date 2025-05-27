21f7b7bf6b72cc268d4fe94ee9dec3d45a872bef
Cloud <9652227+umaim@users.noreply.github.com>
2025-05-27 21:01:44 +0800

    feat: add PWA support
    - add vite-plugin-pwa, @vite-pwa/astro
    - upgrade astro to 5.8.0

--- astro.config.ts
+++ astro.config.ts
@@ -4,12 +4,49 @@ import sitemap from "@astrojs/sitemap";
 import remarkToc from "remark-toc";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
+import AstroPWA from "@vite-pwa/astro";
 import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
   integrations: [
+    AstroPWA({
+      manifest: {
+        name: "Cloud's Time Caplsule",
+        short_name: "Cloud's Blog",
+        start_url: ".",
+        display: "standalone",
+        orientation: "any",
+        description: "Cloud's Blog | Stay Hungry, Stay Foolish.",
+        lang: "zh-cn",
+        icons: [
+          {
+            src: "/web-app-manifest-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/web-app-manifest-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+      },
+      registerType: "autoUpdate",
+      /* enable sw on development */
+      devOptions: {
+        enabled: true,
+        /* other options */
+      },
+      experimental: {
+        directoryAndTrailingSlashHandler: true,
+      },
+    }),
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
     }),
